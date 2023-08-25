cc48b481873948f31d37d69319d4b7cf76407ed7
Mohammed HAJJI BABA <mhajji-b@paul-f4Br3s1.clusters.42paris.fr>
2023-08-25 18:05:36 +0200

    ⚡️ Working progress🔨🔥

--- C01/ex05/Harl.cpp
+++ C01/ex05/Harl.cpp
@@ -6,15 +6,59 @@
 /*   By: mhajji-b <mhajji-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 19:48:04 by mhajji-b          #+#    #+#             */
-/*   Updated: 2023/08/24 19:56:46 by mhajji-b         ###   ########.fr       */
+/*   Updated: 2023/08/25 18:02:53 by mhajji-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <iostream>
 
 Harl::Harl(){
-	
+
 };
 Harl::~Harl(){
-	
+
 };
+void Harl::complain(std::string level)
+{
+	void (Harl::*func[])() = { // Tableau fonction membre
+		&Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+	};
+	
+	std::string word[4] = {"debug", "info", "warning", "error"};
+	for (int i = 0; i < 4; i++)
+	{
+		if (word[i].compare(level) == 0)// Fonctionne comment strcmp
+		{
+			(this->*(func[i]))();
+			break;
+		}
+	}
+}
+
+void Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !"	<<std::endl;
+}
+
+void Harl::info(void)
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put "
+              << "enough bacon in my burger ! If you did, I wouldn't be asking for more !" 
+              << std::endl;
+}
+
+void Harl::warning(void)
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming "
+              << "for years whereas you started working here since last month." 
+              << std::endl;
+}
+
+void Harl::error(void)
+{
+    std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
+}
--- C01/ex05/main.cpp
+++ C01/ex05/main.cpp
@@ -6,13 +6,17 @@
 /*   By: mhajji-b <mhajji-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 19:21:58 by mhajji-b          #+#    #+#             */
-/*   Updated: 2023/08/24 19:47:44 by mhajji-b         ###   ########.fr       */
+/*   Updated: 2023/08/25 17:59:52 by mhajji-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main ()
+int main()
 {
-	
+	Harl test;
+	test.complain("warning");
+	test.complain("debug");
+	test.complain("error");
+	test.complain("info");
 }
