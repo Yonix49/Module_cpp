9cd4a88ac5419130e87b5a9dd6e3443ad0fd4ab8
Mohammed HAJJI BABA <mhajji-b@bess-f3r6s2.clusters.42paris.fr>
2023-10-09 21:25:53 +0200

    good

--- C06/ex02/main.cpp
+++ C06/ex02/main.cpp
@@ -9,57 +9,89 @@
 /*   Updated: 2023/10/06 09:37:46 by mhajji-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "Bureaucrat.hpp"
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <fstream>
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <ctime>   // Pour time
 
-int main()
+Base *generate(void)
 {
-	std::cout << "--------Momo----------" << std::endl;
-	try{
-		Bureaucrat Momo("Momo", 1);
-		std::cout << Momo;
-		while (Momo.getGrade() < 150)
-			Momo.GradeDecrement();
-		std::cout << Momo;
-		while (Momo.getGrade() > 1)
-			Momo.GradeDecrement();
-		std::cout << Momo;
-		Momo.GradeIncrement();
-		std::cout << Momo;
+	int r = rand() % 3; // Compris entre 0 et 2
+	if (r == 0)
+	{
+		std::cout << "Creating A" << std::endl;
+		return (new A);
 	}
-	catch (std::exception &e){
-		std::cout << e.what() << std::endl;
+	else if (r == 1)
+	{
+		std::cout << "Creating B" << std::endl;
+		return (new B);
 	}
-	std::cout << "--------Yoyo----------" << std::endl;
-	try{
-		Bureaucrat Yoyo("Yoyo", 0);
+	else
+	{
+		std::cout << "Creating C" << std::endl;
+		return (new C);
 	}
-	catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	try{
-		Bureaucrat Yoyo("Yoyo", 151);
-	}
-	catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << "--------COMPARE----------" << std::endl;
-	try{
-		Bureaucrat Momo("Momo", 1);
-		Bureaucrat Yoyo("Yoyo", 150);
-		Bureaucrat Yummi(Momo);
-
-		std::cout << Momo << Yoyo << "Yummi: " << Yummi;
-		Momo = Yoyo; 
-		std::cout << Momo << Yoyo << "Yummi: " << Yummi;
-	}
-	catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	return 0;
 };
+
+void identify(Base *p)
+{ // Identifie un type en fonction de son pointeur de base
+	if (dynamic_cast<A *>(p))
+	{
+		std::cout << "A" << std::endl;
+	}
+	else if (dynamic_cast<B *>(p))
+	{
+		std::cout << "B" << std::endl;
+	}
+	else if (dynamic_cast<C *>(p))
+	{
+		std::cout << "C" << std::endl;
+	}
+}
+void identify(Base &p)
+{
+	try
+	{
+		(void)dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
+		return;
+	}
+	catch (const std::exception &e)
+	{
+	}
+	try
+	{
+		(void)dynamic_cast<B &>(p);
+		std::cout << "B" << std::endl;
+		return;
+	}
+	catch (const std::exception &e)
+	{
+	}
+	try
+	{
+		(void)dynamic_cast<C &>(p);
+		std::cout << "C" << std::endl;
+		return;
+	}
+	catch (const std::exception &e)
+	{
+	}
+}
+int main() {
+    std::srand(static_cast<unsigned int>(std::time(0)));
+
+    Base* randomObject = generate();
+
+    std::cout << "Identifying the type: ";
+    identify(randomObject);
+
+    delete randomObject;
+
+    return 0;
+}
